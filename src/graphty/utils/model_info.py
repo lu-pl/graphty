7f08c2a76bf59ba175b6773e2ce76f83f5696c54
Lukas Plank <lupl@tuta.io>
2026-08-03 10:33:56 +0200

    feat: add ModelInfo and ModelInfoRegistry classes
    The change introduces a `ModelInfo` class that outsources information properties about a model that
    are needed by the graphty planner. This encapsulates logic that previously expressed as free
    functions into a single class. Also, this allows to expose info properties as
    `functools.cached_property` to prevent unnecessary re-computation in case of multiple lookups.
    
    The change also introduces a simple `ModelInfoRegsitry`, i.e. a primitive
    `collections.UserDict[type[BaseMode], ModelInfo]` mapping with `__missing__` that acts as a lazy cache for `ModelInfo` objects.

---
+++ src/graphty/utils/model_info.py
@@ -0,0 +1,51 @@
+from collections import UserDict
+from functools import cached_property
+
+from graphty.utils.alias_map import AliasMap
+from graphty.utils.exceptions import InvalidGroupByError
+from graphty.utils.type_utils import is_structured_field_static_type
+from pydantic import BaseModel
+
+
+class ModelInfo[TModel: type[BaseModel]]:
+    def __init__(self, model: TModel, base_cols: set[str]) -> None:
+        self.model = model
+        self.base_cols = base_cols
+
+    @cached_property
+    def alias_map(self) -> AliasMap:
+        return AliasMap(model=self.model, projection=self.base_cols)
+
+    @cached_property
+    def model_projection(self) -> set[str]:
+        return {
+            self.alias_map[field_name]
+            for field_name, field_info in self.model.model_fields.items()
+            if not is_structured_field_static_type(field_info.annotation)
+        }
+
+    @cached_property
+    def group_by(self) -> str | None:
+        if (group_by := self.model.model_config.get("group_by")) is None:
+            return None
+
+        applicable_fields: set[str] = {
+            field_name
+            for field_name, field_info in self.model.model_fields.items()
+            if not is_structured_field_static_type(field_info.annotation)
+        }
+
+        if group_by not in applicable_fields:
+            raise InvalidGroupByError(group_by_value=group_by, model=self.model)
+
+        return self.alias_map[group_by]
+
+
+class ModelInfoRegistry(UserDict[type[BaseModel], ModelInfo]):
+    def __init__(self, base_cols: set[str]) -> None:
+        self.base_cols = base_cols
+        self.data: dict[type[BaseModel], ModelInfo] = {}
+
+    def __missing__(self, key: type[BaseModel]) -> ModelInfo:
+        self.data[key] = ModelInfo(model=key, base_cols=self.base_cols)
+        return self[key]
