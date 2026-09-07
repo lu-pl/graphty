1bd8fed0eef739d510ea9405bee6b11549477811
Lukas Plank <lupl@tuta.io>
2026-09-07 09:50:46 +0200

    feat!: implement extensible aggregation protocol
    The change removes the `graphty.Agg` class and introduces an extensible aggregation protocol
    instead. The protocol ABC `graphty.Aggregation` is defined as `Callable[[pl.Expr], pl.Expr]`;
    `graphty` will try to extract an instance of class `Aggregation` from `Annotated` metadata in a
    field and call this instance on the `pl.Expr` corresponding to the field.
    
    Defaults are `graphty.Reduce(reduction=pl.Expr.first)` for scalar fields and `graphty.Collect()` for
    aggregation fields. I.e. graphty implements its own defaults using the extensible `Aggregation` protocol.
    
    `graphty.Collect` essentially re-implements the now removed `graphty.Agg`.
    
    `graphty.Reduce` exposes a simple interface for controlling scalar reduction in grouping contexts;
    it takes either a string (to be looked up in `pl.Expr`) or a `pl.Expr`-producing `Callable`. For example, the following uses `graphty.Reduce` to calculate the mean in an aggregation:
    
    ```python
    from typing import Annotated
    from pydantic import BaseModel
    from graphty import ConfigDict, ModelMaterializer, Reduce
    
    data = [
        {"x": 1, "y": 2, "z": 3},
        {"x": 1, "y": 3, "z": 4},
        {"x": 2, "y": 4, "z": 5},
    ]
    
    class Model(BaseModel):
        model_config = ConfigDict(group_by="x")
    
        x: int
        y: Annotated[int, Reduce("mean")]
    
    materializer = ModelMaterializer(model=Model, data=data)
    ```
    
    This will produce the following model serialization:
    
    ```json
    [
        {'x': 1, 'y': 2.5},
        {'x': 2, 'y': 4.0}
    ]
    ```
    
    Note that `pl.Reduce` and `pl.Collect` are simply applications of this extensible aggregation
    protocol. Library users can define arbitrary transformation instructions themselves; `graphty` will read any `graphty.Aggregation` subclass and apply it. :)

--- src/graphty/planner.py
+++ src/graphty/planner.py
@@ -15,6 +15,7 @@ from pydantic import BaseModel, Discriminator, Tag
 from pydantic.fields import FieldInfo
 from typing_extensions import TypeForm, get_annotations
 
+from graphty.utils.aggregation import Aggregation, Collect, Reduce
 from graphty.utils.alias_map import AliasMap
 from graphty.utils.exceptions import (
     MissingDiscriminatorError,
@@ -28,7 +29,6 @@ from graphty.utils.type_utils import (
     is_pydantic_model_static_type,
     is_pydantic_model_union_static_type,
 )
-from graphty.utils.types import Agg
 
 
 class ModelUnionDispatch:
@@ -200,16 +200,12 @@ class LazyFramePlanner[TModel: type[BaseModel]]:
 
         if group_by is None:
             if self.model.model_fields:
-                # TODO: when Opacity/planner disengagement is implemented, fields marked
-                # as Opaque should not count as "model fields" for this check — a model
-                # with only Opaque fields should also return the raw frame.
                 return self.lazy_frame.with_columns(
                     *self._generate_expressions(model=self.model)
                 ).drop(self._base_cols.difference(model_projection))
             return self.lazy_frame
 
         return self.lazy_frame.group_by(group_by, maintain_order=True).agg(
-            *[pl.col(col).first() for col in model_projection.difference({group_by})],
             *self._generate_expressions(model=self.model, group_context=True),
         )
 
@@ -222,29 +218,33 @@ class LazyFramePlanner[TModel: type[BaseModel]]:
     ) -> Iterator[pl.Expr]:
         for field_name, field_info in model.model_fields.items():
             annotation = cast(TypeForm, field_info.annotation)
+            aggregation: Aggregation | None = get_metadata(
+                field_info=field_info, cls=Aggregation
+            )
 
             if is_pydantic_model_static_type(annotation):
                 expr: pl.Expr = self._build_model_struct(model=annotation).alias(
                     field_name
                 )
-
-                yield (expr.first() if group_context else expr)
+                reduction: Aggregation = aggregation or Reduce()
+                yield expr if not group_context else reduction(expr)
 
             elif is_pydantic_model_union_static_type(annotation):
                 expr: pl.Expr = (
                     ModelUnionDispatch(
                         # pass full TypeForm for discriminator resolution
-                        type_form=get_annotations(model)[field_name],
+                        type_form=cast(TypeForm, get_annotations(model)[field_name]),
                         discriminator=field_info.discriminator,
                         planner=self,
                     )
                     .compute_model_expr()
                     .alias(field_name)
                 )
-                yield (expr.first() if group_context else expr)
+                reduction: Aggregation = aggregation or Reduce()
+                yield expr if not group_context else reduction(expr)
 
             elif is_parametrized_list_static_type(annotation):
-                (item_annotation,) = get_args(annotation)
+                item_annotation, *_ = get_args(annotation)
 
                 if is_pydantic_model_static_type(item_annotation):
                     inner: pl.Expr = self._build_model_struct(
@@ -265,8 +265,8 @@ class LazyFramePlanner[TModel: type[BaseModel]]:
                     alias_map = self.model_registry[model].alias_map
                     inner: pl.Expr = pl.col(alias_map[field_name])
 
-                agg: Agg = get_metadata(field_info=field_info, cls=Agg) or Agg()
-                expr: pl.Expr = agg.apply_to(inner)
+                agg: Aggregation = aggregation or Collect()
+                expr: pl.Expr = agg(inner)
 
                 partition_value = self.model_registry[model].group_by
                 if partition_value is None:
@@ -278,11 +278,28 @@ class LazyFramePlanner[TModel: type[BaseModel]]:
                     else expr.implode().over(partition_by=partition_value)
                 )
 
+            else:
+                group_by: str | None = self.model_registry[model].group_by
+
+                if group_by is not None:
+                    col = self.model_registry[model].alias_map[field_name]
+
+                    if col != group_by:
+                        reduction: Aggregation = aggregation or Reduce()
+                        expr: pl.Expr = reduction(pl.col(col))
+                        yield expr if group_context else expr.over(group_by)
+
     def _build_model_struct(self, model: type[BaseModel]) -> pl.Expr:
         model_info: ModelInfo = self.model_registry[model]
-        exprs: list[pl.Expr] = [
-            *[pl.col(member) for member in model_info.model_projection],
-            *self._generate_expressions(model=model),
-        ]
+        group_by = model_info.group_by
 
+        col_exprs: list[pl.Expr] = (
+            [pl.col(member) for member in model_info.model_projection]
+            if group_by is None
+            else [pl.col(group_by)]
+        )
+        exprs: list[pl.Expr] = [
+            *col_exprs,
+            *self._generate_expressions(model=model, group_context=False),
+        ]
         return pl.struct(exprs or self._base_cols)
