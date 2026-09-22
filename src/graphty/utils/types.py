23c2a12c7f73800e434b178b33c5999a5e8326c0
Lukas Plank <lupl@tuta.io>
2026-09-22 13:29:56 +0200

    feat: implement planner disengagement for Opaque-annotated fields
    The feature introduces an `Opaque` type that will trigger planner disengagement behavior for
    respectively annotated fields. For model fields annotated like `Annotated[<type>, Opaque()]`, the
    planner will forward the entire projection of the current context and continue - i.e. the planner
    will disengage and not trigger any code paths or recursion for that field.
    
    The rationale for this is (at least) twofold:
    
    First, model implementers might wish to inspect the
    entire projection of the current context and decide on the Python level what to do, e.g. using a
    Pydantic before-validator.
    
    Second, this allows to use arbitrary model unions by handing responsibility and control to the
    calling code. `graphty` usually only supports single model unions (e.g. `Model | None`) and
    discriminated unions because the planner needs a decidable union at planning time. Opaque-annoated
    fields can of course have arbitary union types assigned to them, because the planner will disengage
    and ergo does not have to deal with cyclomatic recursion into every potentially deeply nested model
    of an arbitrary model union. Calling code can then e.g. simply pass such unions to Pydantic, which
    is able to resolve model unions non-recursively.
    
    The following example disengages the planner for the list-aggregated field `y`:
    
    ```python
    data = [
        {"x": 1, "y": 2},
        {"x": 1, "y": 3},
        {"x": 2, "y": 4},
    ]
    
    class Model(BaseModel):
        model_config = ConfigDict(group_by="x")
    
        x: int
        y: Annotated[list[int], Opaque()]
    ```
    
    Without planner disengagement (`y: list[int]`) the above would produce
    
    ```
    [
        {'x': 1, 'y': [2, 3]}
        {'x': 2, 'y': [4]}
    ]
    ```
    
    i.e. the planner will aggregate the indicated columns.
    
    The above example, however, marks the list-typed field as `Opaque`, which will cause the planner to
    disengage and forward the entire projection of the current context:
    
    ```
    [
        {"x": 1, "y": [{"y": 2, "x": 1}, {"y": 3, "x": 1}]},
        {"x": 2, "y": [{"y": 4, "x": 2}]},
    ]
    ```
    
    This allows before-validators to do whatever. :)
    
    Closes #49.

--- src/graphty/utils/types.py
+++ src/graphty/utils/types.py
@@ -3,3 +3,15 @@ from pydantic import ConfigDict as PydanticConfigDict
 
 class ConfigDict(PydanticConfigDict):
     group_by: str
+
+
+class Opaque:
+    """Type for marking a field as opaque to the planner.
+
+    For Pydantic fields typed with `Annotated[<type>, Opaque()]`,
+    the GraphTy planner will forward the entire projection as a pl.Struct
+    and otherwise ignore that field.
+
+    Disengaging the planner for a field with `Opaque`
+    allows a before-validator to act on the raw bindings on the Python level.
+    """
